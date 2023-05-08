5f4baa0b5d31d14d30ccab8e89439eac3332c064
JoeTringali <joet0102@gmail.com>
2023-05-08 15:44:58 -0400

    Change s3 backend key

--- terraform_backend.tf
+++ terraform_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tringali-joe-management"
-    key    = "terraform"
+    key    = "terraform/organization-organizational-units"
     region = "us-east-1"
   }
 }
